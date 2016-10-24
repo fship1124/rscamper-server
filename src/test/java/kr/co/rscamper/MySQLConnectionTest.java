89bd61e9a2763cd055a2894d9eaceb0cae4d7b16
Kim Ho Dong <fship1124@gmail.com>
2016-10-24 15:39:25 +0900

    스프링 프로젝트 기본구조 초안

---
+++ src/test/java/kr/co/rscamper/MySQLConnectionTest.java
@@ -0,0 +1,23 @@
+package kr.co.rscamper;
+
+import java.sql.DriverManager;
+import org.junit.Test;
+import java.sql.Connection;
+
+public class MySQLConnectionTest {
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/rscamper";
+	private static final String USER = "rscamper";
+	private static final String PW = "1004";
+	
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(DRIVER);
+		
+		try (Connection con = DriverManager.getConnection(URL, USER, PW)) {
+			System.out.println(con);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
