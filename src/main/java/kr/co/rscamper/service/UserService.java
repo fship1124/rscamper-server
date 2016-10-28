6574e0045ff8e2022119f9e7b15927f3ae06cc5d
Song <iamsjh1017@gmail.com>
2016-10-28 20:34:45 +0900

    TODO

--- src/main/java/kr/co/rscamper/service/UserService.java
+++ src/main/java/kr/co/rscamper/service/UserService.java
@@ -5,6 +5,16 @@ import kr.co.rscamper.domain.UserVO;
 public interface UserService {
 	public UserVO selectMainByUidComment(String userUid) throws Exception;
 	
-	/** 회원 정보 입력 */
+	/** [App] 회원 가입 */
 	public void insertUser(UserVO user) throws Exception;
+	
+	/** TODO: [App] UID로 회원 정보 가져오기 */
+	
+	/** TODO: [App] 회원 리스트 가져오기 */
+	
+	/** TODO: [App] 회원 정보 수정 */
+	
+	/** TODO: [App] 회원 탈퇴 */
+	
+	
 }
