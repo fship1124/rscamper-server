4a28665d7de62972b7d8aedf2069865e2f5d326e
Song <iamsjh1017@gmail.com>
2016-10-28 14:07:44 +0900

    jackson 라이브러리추가, 한글 인코딩 추가, 회원정보인력 컨트롤러추가

--- src/main/java/kr/co/rscamper/controller/UserController.java
+++ src/main/java/kr/co/rscamper/controller/UserController.java
@@ -1,5 +1,7 @@
 package kr.co.rscamper.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,16 +10,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.rscamper.domain.UserVO;
+import kr.co.rscamper.service.UserService;
 
 @Controller
 @RequestMapping("/user/*")
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
+	@Inject
+	private UserService service;
 	
 	@RequestMapping(value="/insert", method = RequestMethod.POST)
 	public @ResponseBody void insert(UserVO user) throws Exception{
-		
+		System.out.println(user.toString());
+		service.insertUser(user);
 	}
 	
 }
