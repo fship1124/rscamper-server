6183ed3bece32b7d0184b85b11cbea1d2583debe
song <iamsjh1017@gmail.com>
2016-11-23 09:03:45 +0900

    일정 하는중

---
+++ src/main/java/kr/co/rscamper/controller/TourPlanController.java
@@ -0,0 +1,47 @@
+package kr.co.rscamper.controller;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.rscamper.domain.TourPlanParamVO;
+import kr.co.rscamper.domain.TourPlanSpotParamVO;
+import kr.co.rscamper.domain.TourPlanVO;
+import kr.co.rscamper.service.TourPlanService;
+
+@Controller
+@RequestMapping("/tourPlan")
+public class TourPlanController {
+
+	private static final Logger logger = LoggerFactory.getLogger(TourPlanController.class);
+	
+	@Inject
+	private TourPlanService tourPlanService;
+	
+	@RequestMapping(value = "/select/tourPlanList", method = RequestMethod.POST)
+	public @ResponseBody Map<String, Object> selectTourPlanList(TourPlanParamVO tourPlanParam) throws Exception {
+		return tourPlanService.selectTourPlanList(tourPlanParam);
+	}
+	
+	@RequestMapping(value = "/insert/tourPlan", method = RequestMethod.POST)
+	public @ResponseBody void insertTourPlan(TourPlanVO tourPlan) throws Exception {
+		tourPlanService.insertTourPlan(tourPlan);
+	}
+	
+	@RequestMapping(value = "/select/spotList", method = RequestMethod.GET)
+	public @ResponseBody Map<String, Object> selectSpotList(TourPlanSpotParamVO tourPlanSpotParam) throws Exception {
+		return tourPlanService.selectSpotList(tourPlanSpotParam);
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/kr/co/rscamper/domain/TourPlanSpotParamVO.java
@@ -0,0 +1,76 @@
+package kr.co.rscamper.domain;
+
+import java.util.Date;
+
+public class TourPlanSpotParamVO {
+	
+	// 페이징
+	private int amount;
+	private int pageNo;
+	private int startRow;
+	
+	// 정렬
+	private String standard;
+	private String order;
+	
+	// 검색 및 필터
+	private String word;
+	private String category;
+	
+	@Override
+	public String toString() {
+		return "TourPlanSpotParamVO [amount=" + amount + ", pageNo=" + pageNo + ", startRow=" + startRow + ", standard="
+				+ standard + ", order=" + order + ", word=" + word + ", category=" + category + "]";
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getPageNo() {
+		return pageNo;
+	}
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	public int getStartRow() {
+		return startRow;
+	}
+	public void setStartRow(int startRow) {
+		this.startRow = startRow;
+	}
+	public String getStandard() {
+		return standard;
+	}
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+	public String getOrder() {
+		return order;
+	}
+	public void setOrder(String order) {
+		this.order = order;
+	}
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+}
