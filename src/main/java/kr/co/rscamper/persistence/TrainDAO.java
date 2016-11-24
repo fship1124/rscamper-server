529e853a6c97d9f3166a73ee2152f864c86f581f
daechun <kdh487@gmail.com>
2016-11-24 09:43:32 +0900

    dh commit

--- src/main/java/kr/co/rscamper/persistence/TrainDAO.java
+++ src/main/java/kr/co/rscamper/persistence/TrainDAO.java
@@ -7,4 +7,25 @@ import kr.co.rscamper.domain.TrainVO;
 public interface TrainDAO {
 	
 	public List<TrainVO> list()throws Exception;
+
+	public String selectDepPlaceId(String depPlaceId)throws Exception;
+
+	public String selectArrPlaceId(String arrPlaceId)throws Exception;
+
+	public List<TrainVO> trainSearch(String stationTitle)throws Exception;
+
+
+//	public String selectSaemaeul(TrainVO train)throws Exception;
+//
+//	public String selectMugunghwa(TrainVO train)throws Exception;
+//
+//	public String selectTonggeun(TrainVO train)throws Exception;
+//
+//	public String selectNuriro(TrainVO train)throws Exception;
+//
+//	public String selectItxchungchun(TrainVO train)throws Exception;
+//
+//	public String selectItxsaemaeul(TrainVO train)throws Exception;
+
+
 }
