58e5610bfd6fa12e7ef3cd13dcc69b6f8cbf1b40
daechun <kdh487@gmail.com>
2016-12-05 15:19:26 +0900

    asdf

--- src/main/java/kr/co/rscamper/controller/TrainMapController.java
+++ src/main/java/kr/co/rscamper/controller/TrainMapController.java
@@ -1,14 +1,26 @@
 package kr.co.rscamper.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.rscamper.domain.TrainMapVO;
+import kr.co.rscamper.service.TrainMapService;
 
 @Controller
 @RequestMapping("/trainMap/*")
 public class TrainMapController {
+	
+	@Inject
+	private TrainMapService service;
 	
 	private static final Logger logger = LoggerFactory.getLogger(TrainMapController.class);
 	
@@ -17,5 +29,21 @@ public class TrainMapController {
 		logger.info("/trainMap > home");
 		
 		return "redirect:http://localhost:80/rscamper-web/views/train/trainMap.jsp";
+	}
+	
+	@RequestMapping(value="/mapTrain", method = RequestMethod.GET)
+	public @ResponseBody List<TrainMapVO> list() throws Exception {
+		logger.info("trainMap");
+		List<TrainMapVO> list = new ArrayList<>();
+		list = service.list();
+		return list;
+	}
+	
+	@RequestMapping(value="/trainMapLine", method = RequestMethod.GET)
+	public @ResponseBody List<TrainMapVO> trainLineMapList() throws Exception {
+		logger.info("trainMapLine");
+		List<TrainMapVO> result = new ArrayList<>();
+		result = service.trainLineMapList();
+		return result;
 	}
 }
