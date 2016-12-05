217a06fd3b50a0885af928a9f66ac655c3ddae9e
sungjukk <lsj0147@naver.com>
2016-12-05 14:30:51 +0900

    수정

--- src/main/java/kr/co/rscamper/domain/TourPlanVO.java
+++ src/main/java/kr/co/rscamper/domain/TourPlanVO.java
@@ -10,14 +10,18 @@ public class TourPlanVO {
 	private Date departureDate;
 	private Date arriveDate;
 	private String title;
-	private int budget;
+	private int budGet;
 	private int picture;
 	private String period;
 	private String strapline;
 	private String introduce;
+
 	private Date regDate;
 	private int isOpen;
+	private RecordCoverVO cover;
 	
+
+
 	// 카운트
 	private int likeCnt;
 	private int commentCnt;
@@ -34,154 +38,174 @@ public class TourPlanVO {
 	
 	// 페이징
 	private int totalPages;
-	
 
-	public String getFilePath() {
-		return filePath;
+	public int getRecordNo() {
+		return recordNo;
 	}
 
+	public void setRecordNo(int recordNo) {
+		this.recordNo = recordNo;
+	}
 
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
+	public String getUserUid() {
+		return userUid;
+	}
+
+	public void setUserUid(String userUid) {
+		this.userUid = userUid;
+	}
+
+	public Date getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(Date departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public Date getArriveDate() {
+		return arriveDate;
+	}
+
+	public void setArriveDate(Date arriveDate) {
+		this.arriveDate = arriveDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getBudGet() {
+		return budGet;
+	}
+
+	public void setBudGet(int budGet) {
+		this.budGet = budGet;
+	}
+
+	public int getPicture() {
+		return picture;
+	}
+
+	public void setPicture(int picture) {
+		this.picture = picture;
+	}
+
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public String getStrapline() {
+		return strapline;
+	}
+
+	public void setStrapline(String strapline) {
+		this.strapline = strapline;
+	}
+
+	public String getIntroduce() {
+		return introduce;
+	}
+
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
 	}
 
 	public int getIsOpen() {
 		return isOpen;
 	}
 
-
 	public void setIsOpen(int isOpen) {
 		this.isOpen = isOpen;
 	}
 
-
-	public String getPhotoUrl() {
-		return photoUrl;
+	public RecordCoverVO getCover() {
+		return cover;
 	}
 
-
-	public void setPhotoUrl(String photoUrl) {
-		this.photoUrl = photoUrl;
+	public void setCover(RecordCoverVO cover) {
+		this.cover = cover;
 	}
-	
+
+	public int getLikeCnt() {
+		return likeCnt;
+	}
+
+	public void setLikeCnt(int likeCnt) {
+		this.likeCnt = likeCnt;
+	}
+
+	public int getCommentCnt() {
+		return commentCnt;
+	}
+
+	public void setCommentCnt(int commentCnt) {
+		this.commentCnt = commentCnt;
+	}
+
+	public int getLocationCnt() {
+		return locationCnt;
+	}
+
+	public void setLocationCnt(int locationCnt) {
+		this.locationCnt = locationCnt;
+	}
+
 	public int getPostCnt() {
 		return postCnt;
 	}
-
 
 	public void setPostCnt(int postCnt) {
 		this.postCnt = postCnt;
 	}
 
-
-	public int getLikeCnt() {
-		return likeCnt;
-	}
-	public void setLikeCnt(int likeCnt) {
-		this.likeCnt = likeCnt;
-	}
-	public int getCommentCnt() {
-		return commentCnt;
-	}
-	public void setCommentCnt(int commentCnt) {
-		this.commentCnt = commentCnt;
-	}
-	public int getLocationCnt() {
-		return locationCnt;
-	}
-	public void setLocationCnt(int locationCnt) {
-		this.locationCnt = locationCnt;
-	}
-	public int getTotalPages() {
-		return totalPages;
-	}
-	public void setTotalPages(int totalPages) {
-		this.totalPages = totalPages;
-	}
 	public String getDisplayName() {
 		return displayName;
 	}
+
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 	}
-	public String getPeriod() {
-		return period;
-	}
-	public void setPeriod(String period) {
-		this.period = period;
-	}
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-	public String getStrapline() {
-		return strapline;
-	}
-	public void setStrapline(String strapline) {
-		this.strapline = strapline;
-	}
-	public String getIntroduce() {
-		return introduce;
-	}
-	public void setIntroduce(String introduce) {
-		this.introduce = introduce;
-	}	
-	public int getRecordNo() {
-		return recordNo;
-	}
-	public void setRecordNo(int recordNo) {
-		this.recordNo = recordNo;
-	}
-	public String getUserUid() {
-		return userUid;
-	}
-	public void setUserUid(String userUid) {
-		this.userUid = userUid;
-	}
-	public Date getDepartureDate() {
-		return departureDate;
-	}
-	public void setDepartureDate(Date departureDate) {
-		this.departureDate = departureDate;
-	}
-	public Date getArriveDate() {
-		return arriveDate;
-	}
-	public void setArriveDate(Date arriveDate) {
-		this.arriveDate = arriveDate;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public int getBudGet() {
-		return budget;
-	}
-	public void setBudget(int budget) {
-		this.budget = budget;
-	}
-	public int getPicture() {
-		return picture;
-	}
-	public void setPicture(int picture) {
-		this.picture = picture;
+
+	public String getPhotoUrl() {
+		return photoUrl;
 	}
 
-
-	@Override
-	public String toString() {
-		return "TourPlanVO [recordNo=" + recordNo + ", userUid=" + userUid + ", departureDate=" + departureDate
-				+ ", arriveDate=" + arriveDate + ", title=" + title + ", budget=" + budget + ", picture=" + picture
-				+ ", period=" + period + ", strapline=" + strapline + ", introduce=" + introduce + ", regDate="
-				+ regDate + ", likeCnt=" + likeCnt + ", commentCnt=" + commentCnt + ", locationCnt=" + locationCnt
-				+ ", postCnt=" + postCnt + ", displayName=" + displayName + ", filePath=" + filePath + ", totalPages="
-				+ totalPages + "]";
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
 	}
 
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
 	
 	
 }
